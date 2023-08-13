f20fe85c476fdfe48434dfd8c52922e34fe73d0f
Eshan <111144332+ImeshR@users.noreply.github.com>
2023-08-13 11:06:24 +0530

    frontend folder structure set

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from './pages/home';
+import footer from './components/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    {/* <Route path="*" element={<Error />} /> */}
+    <Route index element={<Home />} />
+  </Routes>
+</BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
